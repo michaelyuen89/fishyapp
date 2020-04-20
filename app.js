6fd24b08993d70c0ebf88cb0dc10f978cfc07880
Emily Wu <huijiawu90@gmail.com>
2020-04-20 10:13:22 -0400

    Start setting up user auth backend

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser");
 // const passport = require("passport");
 
 const users = require("./routes/api/users");
-// const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -18,11 +17,10 @@ app.get("/", (req, res) => res.send("Hello Non-Fishers!!"));
 // app.use(passport.initialize());
 // require("./config/passport")(passport);
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.use("/api/users", users);
-// app.use("/api/tweets", tweets);
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the fishermens route" }));
+router.get("/test", (req, res) => res.json({ msg: "This is the fishers route" }));
+
+const User = require('../../models/User');
+
+router.post("/register", (req, res) => {
+  // Check to make sure nobody has already registered with a duplicate email
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      // Throw a 400 error if the email address already exists
+      return res
+        .status(400)
+        .json({ email: "A user has already registered with this address" });
+    } else {
+      // Otherwise create a new user
+      const newUser = new User({
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
