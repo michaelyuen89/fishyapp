461766ebc7d3a262ad51413d75a62bdc60093554
michaelyuen89 <michaelyuen89@gmail.com>
2020-04-22 13:43:13 -0400

    Test heroku push

--- app.js
+++ app.js
@@ -13,14 +13,6 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello Non-Fishers!!"));
 
-app.use(passport.initialize());
-require("./config/passport")(passport);
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use("/api/users", users);
-
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
@@ -30,6 +22,15 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
