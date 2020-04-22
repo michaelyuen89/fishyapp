1a1fc67d1630c0bf5a722a3a920aa7645ffa5c2f
michaelyuen89 <michaelyuen89@gmail.com>
2020-04-22 10:35:40 -0400

    test app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use("/api/users", users);
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
+  console.log("we're in production");
   app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
