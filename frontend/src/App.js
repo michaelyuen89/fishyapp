2dc1df14fc878419cf99a22b286d89605b5e61c8
michaelyuen89 <michaelyuen89@gmail.com>
2020-04-21 23:12:02 -0400

    Change Heroku App setup location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const path = require('path');
 
 function App() {
   return (
@@ -24,3 +25,10 @@ function App() {
 }
 
 export default App;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
