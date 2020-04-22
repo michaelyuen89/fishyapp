baa2927346b2f92a876d99327a89ef9c66d35d17
michaelyuen89 <michaelyuen89@gmail.com>
2020-04-21 23:06:27 -0400

    Setup Heroku

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {AuthRoute, ProtectedRoute} from '../util/route_util';
 import { Route, Switch} from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
+const path = require('path');
 
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
@@ -19,3 +20,11 @@ const App = () => (
 );
 
 export default App;
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
