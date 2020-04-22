757e03f0739fe74864a771a9f90e9ff22501e166
michaelyuen89 <michaelyuen89@gmail.com>
2020-04-22 09:31:27 -0400

    Change public to build in app.js

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ const path = require('path');
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/public'));
+  app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
 
