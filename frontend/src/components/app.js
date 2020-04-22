32102161ddfe6a376ea80216b7b874387eded3ea
Shaohua Xiao <joexiao97@gmail.com>
2020-04-21 23:33:24 -0400

    new file upload component not finished

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,6 +6,7 @@ import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import NewFileUpload from './newFileUpload';
 
 const App = () => (
     <div>
@@ -14,6 +15,7 @@ const App = () => (
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
             <Route path="/" component={MainPage} />
+            <Route path="/api/document/upload" component={NewFileUpload} />
         </Switch>
     </div>
 );
