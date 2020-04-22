1adc9194cf2d5263fd113e2f6df3ade7e6a9f0da
michaelyuen89 <michaelyuen89@gmail.com>
2020-04-21 23:33:53 -0400

    Change Heroku Setup location again

--- app.js
+++ app.js
@@ -24,3 +24,15 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+const path = require('path');
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
