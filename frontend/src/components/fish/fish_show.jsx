8aa1801b418d15adbcad955edcf8f2ec9a14d7e7
Emily Wu <huijiawu90@gmail.com>
2020-04-24 10:17:06 -0400

    Style fish show page

--- frontend/src/components/fish/fish_show.jsx
+++ frontend/src/components/fish/fish_show.jsx
@@ -8,6 +8,7 @@ import noFish from "./nofish.jpg"
 import FishUploadContainer from "./fish_photo_upload_container";
 import FishShowsPhotosContainer from "./fish_shows_photos_container";
 
+
 class FishShow extends React.Component {
   constructor(props) {
     super(props);
@@ -123,64 +124,58 @@ class FishShow extends React.Component {
     } else {
       return (
         <>
-          {/* <div className="photo-upload-form">
-            <h1>Photo Upload Form</h1>
+          <div className="fish-index-page main-page-container">
+            <img src={splashImage} />
             <div>
-              <form action="" onSubmit={this.handleUpload}>
-                <br />
-                <input
-                  type="file"
-                  onChange={this.handleFile}
-                  ref={this.fileInput}
-                />
-                <br />
-                <input type="submit" value="Upload" />
-              </form>
-            </div>
-          </div> */}
-          <div className="fish-show-page">
-            <div className="fish-left">
-            <div className="fish-show-name">{this.targetFish.name}</div>
-            <div className="fish-show-text">
-              <div className="fish-show-info">
-                {/* <span>Description</span>
-                    <div className="fish-show-description">
-                      {this.targetFish.description}
-                    </div> */}
-                <div className="fish-show-size">
-                  <span>Min Legal Size: </span>
-                  {/* <div className="fish-show-size-min"> */}
-                    {this.targetFish.minLegalSize ? this.targetFish.minLegalSize : "None"}
-                  {/* </div> */}
-                  <span>Max Legal Size: </span>
-                  <div className="fish-show-size-max">
-                    {this.targetFish.maxLegalSize}
+            <div className="fish-show-name ">{this.targetFish.name}</div>
+            <div className="fish-show-page ">
+              <div className="fish-left">
+                <div className="fish-show-text">
+                  <div className="fish-show-img">
+                    <img
+                      src={
+                        this.props.fishPhotos[0]
+                          ? this.props.fishPhotos[0].fileLink
+                          : noFish
+                      }
+                      alt={this.targetFish.name}
+                    />
+                  </div>
+                  <div className="fish-show-info">
+                    <div className="fish-show-size">
+                      <div>
+                        <span>Min Legal Size: </span>
+                        {this.targetFish.minLegalSize
+                          ? this.targetFish.minLegalSize
+                          : "None"}
+                      </div>
+                      <div>
+                        <span>Max Legal Size: </span>
+                        {this.targetFish.maxLegalSize
+                          ? this.targetFish.maxLegalSize
+                          : "None"}
+                      </div>
+                      <div>
+                        <span>Max Allowed Amount: </span>
+                        {this.targetFish.maxPossession
+                          ? this.targetFish.maxPossession
+                          : "None"}
+                      </div>
+                      <div>
+                        <span>Description: </span>
+                        {this.targetFish.description
+                          ? this.targetFish.description
+                          : "No description provided"}
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <span>Max Allowed Amount: </span>
-                <div className="fish-show-max">
-                  {this.targetFish.maxPossession}
                 </div>
+              <div className="fish-gmaps-section">
+                <div>Places to fish for {this.targetFish.name}:</div>
+                <Map />
               </div>
-
-              <div>
-                <img
-                  className="fish-show-img"
-                  src={this.props.fishPhotos[0] ? this.props.fishPhotos[0].fileLink : noFish}
-                  alt="Fish display picture"
-                />
               </div>
-
-              <div className="fish-show-desc-box">
-                <div className="fish-show-desc">Description</div>
-                <div className="fish-show-description">
-                  {this.targetFish.description}
-                </div>
-              </div>
-            </div>
-            </div>
-            <div className="fish-gmaps-section">
-              <Map />
             </div>
           </div>
           <FishShowsPhotosContainer />
--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -20,10 +20,10 @@ class NavBar extends React.Component {
                 <div className="nav-links logged-in">
                     <div className="nav-links-left">
                         <div>
-                            <Link to={'/fishes'}>All fish</Link>
+                            <Link to={'/profile'}>Profile</Link>
                         </div>
                         <div>
-                            <Link to={'/profile'}>Profile</Link>
+                            <Link to={'/fishes'}>All fish</Link>
                         </div>
                         <div>
                             <Link to={'/new_fish'}>Add a fish</Link>
