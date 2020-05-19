c1e9509e14ea70d279c30c39a9a02f31d3a0958b
Shaohua Xiao <joexiao97@gmail.com>
2020-05-19 16:26:54 -0400

    boiler

--- frontend/src/components/team/team_page.jsx
+++ frontend/src/components/team/team_page.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import "./team_page.css";
-import "./joe.jpg";
-import "./elijah.jpg";
-import "./emily.jpg";
-import "./mike.jpg";
+import joe from "./joe.jpg";
+import eli from "./elijah.jpg";
+import emily from "./emily.jpg";
+import mike from "./mike.jpg";
 
 class TeamPage extends React.Component {
 
 
     render() {
         return (
-            <div className="team-page-container">
-                {/* <div> */}
-                    <span>Meet our team!</span>
-                    <div className="team-member">
-                        Elijah
-                    </div>
-                    <div className="team-member">
-                        Emily
-                    </div>
-                    <div className="team-member">
-                        Joe
-                    </div>
-                    <div className="team-member">
-                        Michael
-                    </div>
-                {/* </div> */}
+          <div className="team-page-container">
+            <div className="team-page-title">Meet our team!</div>
+
+            <div className="team-member-container">
+              <div className="team-member-picture">
+                <img src={eli} alt="team-member-elijah" />
+              </div>
+              <div className="team-member-info">
+                <div className="team-member-name">Elijah</div>
+                <div className="team-member-op">
+                  <a target="_blank" href="https://github.com/Sunghan11">Github</a>
+                  <a target="_blank" href="https://www.linkedin.com/in/elijah-nam-6a412b25/">LinkedIn</a>
+                  <a target="_blank" href="https://angel.co/u/elijah-nam">AngelList</a>
+                </div>
+              </div>
             </div>
-        )
+
+            <div className="team-member">Emily</div>
+            <div className="team-member">Joe</div>
+            <div className="team-member">Michael</div>
+          </div>
+        );
     }
 }
 
